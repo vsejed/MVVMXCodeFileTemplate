585d320b4fab3299fa825e68ca24fe20328c34ab
Martin Hubner <martin.hubner@vsejed.cz>
2018-07-25 14:06:56 +0200

    UIViewController template added

---
+++ MVVM Template/UIViewController.xctemplate/UIViewController/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,20 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___VARIABLE_sceneName___ViewController: ___VARIABLE_viewControllerSubclass___ {
+
+    init() {
+        super.init(nibName: "___VARIABLE_sceneName___ViewController", bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
